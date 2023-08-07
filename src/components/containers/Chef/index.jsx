f3a02bb511e44267b3d52e3137b3e8fcf4efa56e
Kihito <benner.andrey.loon@gmail.com>
2023-08-08 00:58:25 +0300

    Half project

---
+++ src/components/containers/Chef/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const Chefs = () => {
+
+    return (
+        <div {...{className: 'chefs'}}>
+
+        </div>
+    )
+}
+
+export default Chefs;
